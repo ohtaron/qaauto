54329b88a15b6bfb694b6aa73dc2e997ad299fe8
ohtaron <ohta@i.ua>
2019-03-21 21:33:44 +0200

    new

--- qaauto-4.02.2019/src/main/java/BadCodeExample.java
+++ qaauto-4.02.2019/src/main/java/BadCodeExample.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BadCodeExample {
 
@@ -9,8 +12,29 @@ public class BadCodeExample {
         System.out.println("Hello World");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Selenium", (Keys.ENTER));
-        
+
+        String searchTerm = "Selenium";
+        //WebElement searchField = driver.findElement(By.name("q"));
+        WebElement searchField = driver.findElement(By.xpath("//input[@name='q']"));
+        searchField.sendKeys(searchTerm);
+        searchField.sendKeys((Keys.ENTER));
+        List<WebElement> searchResults = driver.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
+
+        System.out.println(searchResults.size());
+
+        for (WebElement searchResult : searchResults) {
+            String searchResultString = searchResult.getText();
+            System.out.println(searchResult.getText());
+            //System.out.println(searchResultString);
+
+            if (searchResultString.toLowerCase().contains(searchTerm.toLowerCase())) {
+                System.out.println("SearchTerm found");
+            } else {
+                System.out.println("SearchTerm not found");
+            }
+        }
+
+
     }
 
 }
